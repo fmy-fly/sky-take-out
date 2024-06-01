15c7fa99c9fab2ca7213720d3e12b39d0d08fd5e
dream_Flying <84890202+fmy-fly@users.noreply.github.com>
2024-06-01 18:27:11 +0800

    全部代码，后续会删减

--- sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
+++ sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
@@ -26,16 +26,22 @@ public class GlobalExceptionHandler {
         log.error("异常信息：{}", ex.getMessage());
         return Result.error(ex.getMessage());
     }
+
+    /**
+     * 处理SQL异常
+     * @param ex
+     * @return
+     */
     @ExceptionHandler
     public Result exceptionHandler(SQLIntegrityConstraintViolationException ex){
+        //Duplicate entry 'zhangsan' for key 'employee.idx_username'
         String message = ex.getMessage();
-        if (message.contains("Duplicate entry")){
+        if(message.contains("Duplicate entry")){
             String[] split = message.split(" ");
             String username = split[2];
-            String msg = username + MessageConstant.ALREADY_EXIST;
+            String msg = username + MessageConstant.ALREADY_EXISTS;
             return Result.error(msg);
-        }
-        else{
+        }else{
             return Result.error(MessageConstant.UNKNOWN_ERROR);
         }
     }
--- sky-server/src/main/java/com/sky/service/EmployeeService.java
+++ sky-server/src/main/java/com/sky/service/EmployeeService.java
@@ -15,13 +15,36 @@ public interface EmployeeService {
      */
     Employee login(EmployeeLoginDTO employeeLoginDTO);
 
-    void save(EmployeeDTO employeeDto);
+    /**
+     * 新增员工
+     * @param employeeDTO
+     */
+    void save(EmployeeDTO employeeDTO);
 
-    PageResult pagequery(EmployeePageQueryDTO employeePageQueryDTO);
+    /**
+     * 分页查询
+     * @param employeePageQueryDTO
+     * @return
+     */
+    PageResult pageQuery(EmployeePageQueryDTO employeePageQueryDTO);
 
-    void startOrstop(Integer status, Long id);
+    /**
+     * 启用禁用员工账号
+     * @param status
+     * @param id
+     */
+    void startOrStop(Integer status, Long id);
 
+    /**
+     * 根据id查询员工
+     * @param id
+     * @return
+     */
     Employee getById(Long id);
 
+    /**
+     * 编辑员工信息
+     * @param employeeDTO
+     */
     void update(EmployeeDTO employeeDTO);
 }
