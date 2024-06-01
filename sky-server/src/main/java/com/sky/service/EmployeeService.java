7087f1650d9b643d9a4b9e972e02dcccbb857ea2
dream_Flying <84890202+fmy-fly@users.noreply.github.com>
2024-06-01 18:45:52 +0800

    Revert "全部代码，后续会删减"
    This reverts commit 15c7fa99c9fab2ca7213720d3e12b39d0d08fd5e.

--- sky-server/src/main/java/com/sky/service/EmployeeService.java
+++ sky-server/src/main/java/com/sky/service/EmployeeService.java
@@ -15,36 +15,13 @@ public interface EmployeeService {
      */
     Employee login(EmployeeLoginDTO employeeLoginDTO);
 
-    /**
-     * 新增员工
-     * @param employeeDTO
-     */
-    void save(EmployeeDTO employeeDTO);
+    void save(EmployeeDTO employeeDto);
 
-    /**
-     * 分页查询
-     * @param employeePageQueryDTO
-     * @return
-     */
-    PageResult pageQuery(EmployeePageQueryDTO employeePageQueryDTO);
+    PageResult pagequery(EmployeePageQueryDTO employeePageQueryDTO);
 
-    /**
-     * 启用禁用员工账号
-     * @param status
-     * @param id
-     */
-    void startOrStop(Integer status, Long id);
+    void startOrstop(Integer status, Long id);
 
-    /**
-     * 根据id查询员工
-     * @param id
-     * @return
-     */
     Employee getById(Long id);
 
-    /**
-     * 编辑员工信息
-     * @param employeeDTO
-     */
     void update(EmployeeDTO employeeDTO);
 }
--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -50,7 +50,8 @@ public class EmployeeServiceImpl implements EmployeeService {
         }
 
         //密码比对
-        //对前端传过来的明文密码进行md5加密处理
+        // 后期需要进行md5加密，然后再进行比对
+
         password = DigestUtils.md5DigestAsHex(password.getBytes());
         if (!password.equals(employee.getPassword())) {
             //密码错误
@@ -66,66 +67,56 @@ public class EmployeeServiceImpl implements EmployeeService {
         return employee;
     }
 
-    /**
-     * 新增员工
-     *
-     * @param employeeDTO
-     */
-    public void save(EmployeeDTO employeeDTO) {
+
+    public void save(EmployeeDTO employeeDto) {
+        System.out.println("当前线程的id:" + Thread.currentThread().getId());
+
         Employee employee = new Employee();
+        // 对象属性拷贝
+        BeanUtils.copyProperties(employeeDto,employee);
 
-        //对象属性拷贝
-        BeanUtils.copyProperties(employeeDTO, employee);
-
-        //设置账号的状态，默认正常状态 1表示正常 0表示锁定
+        // 设置账号的状态，默认正常状态 1表示正常 0表示锁定
         employee.setStatus(StatusConstant.ENABLE);
 
-        //设置密码，默认密码123456
+        // 设置默认密码123456
         employee.setPassword(DigestUtils.md5DigestAsHex(PasswordConstant.DEFAULT_PASSWORD.getBytes()));
 
-        //设置当前记录的创建时间和修改时间
-        //employee.setCreateTime(LocalDateTime.now());
-        //employee.setUpdateTime(LocalDateTime.now());
+        //设置当前记录的 创建时间和修改时间
+        employee.setCreateTime(LocalDateTime.now());
+        employee.setUpdateTime(LocalDateTime.now());
+
 
         //设置当前记录创建人id和修改人id
-        //employee.setCreateUser(BaseContext.getCurrentId());
-        //employee.setUpdateUser(BaseContext.getCurrentId());
+        employee.setCreateUser(BaseContext.getCurrentId());
+        employee.setUpdateUser(BaseContext.getCurrentId());
+
 
         employeeMapper.insert(employee);
     }
 
+
     /**
-     * 分页查询
-     *
+     * 员工分页查询
      * @param employeePageQueryDTO
      * @return
      */
-    public PageResult pageQuery(EmployeePageQueryDTO employeePageQueryDTO) {
-        // select * from employee limit 0,10
-        //开始分页查询
+    @Override
+    public PageResult pagequery(EmployeePageQueryDTO employeePageQueryDTO) {
+        //用的MB框架的PageHelper插件提供的方法startPage(a, b)
+        //a页码, b每页的记录数
         PageHelper.startPage(employeePageQueryDTO.getPage(), employeePageQueryDTO.getPageSize());
-
+        //DTO里面有name， page， pageSize
+        //插件又要求返回的是page，那么用完mapper的pageQuery方法后需要再用page来装
         Page<Employee> page = employeeMapper.pageQuery(employeePageQueryDTO);
 
+        //page要回到pageResult对象，直接调用方法拿回去
         long total = page.getTotal();
-        List<Employee> records = page.getResult();
-
-        return new PageResult(total, records);
+        List<Employee> recods = page.getResult();
+        return new PageResult(total, recods);
     }
 
-    /**
-     * 启用禁用员工账号
-     *
-     * @param status
-     * @param id
-     */
-    public void startOrStop(Integer status, Long id) {
-        // update employee set status = ? where id = ?
-
-        /*Employee employee = new Employee();
-        employee.setStatus(status);
-        employee.setId(id);*/
-
+    @Override
+    public void startOrstop(Integer status, Long id) {
         Employee employee = Employee.builder()
                 .status(status)
                 .id(id)
@@ -134,30 +125,21 @@ public class EmployeeServiceImpl implements EmployeeService {
         employeeMapper.update(employee);
     }
 
-    /**
-     * 根据id查询员工
-     *
-     * @param id
-     * @return
-     */
+    @Override
     public Employee getById(Long id) {
         Employee employee = employeeMapper.getById(id);
-        employee.setPassword("****");
+        employee.setPassword(("****"));
         return employee;
     }
 
-    /**
-     * 编辑员工信息
-     *
-     * @param employeeDTO
-     */
+    @Override
     public void update(EmployeeDTO employeeDTO) {
         Employee employee = new Employee();
-        BeanUtils.copyProperties(employeeDTO, employee);
-
-        //employee.setUpdateTime(LocalDateTime.now());
-        //employee.setUpdateUser(BaseContext.getCurrentId());
-
+        BeanUtils.copyProperties(employeeDTO,employee);
+        employee.setUpdateTime(LocalDateTime.now());
+        employee.setUpdateUser(BaseContext.getCurrentId());
         employeeMapper.update(employee);
     }
+
+
 }
